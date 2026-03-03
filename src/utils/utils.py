bb0a664ef9504b73b0e06d8492d233cd138c8810
santanikolaus <lewehofer@gmail.com>
2026-03-03 19:58:31 +0100

    migrate Hydra

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,29 @@
 from math import prod
+from typing import List
+
+import lightning as L
+from hydra.utils import instantiate
+from omegaconf import DictConfig
+
+
+def instantiate_loggers(logger_cfg) -> List:
+    loggers = []
+    if not logger_cfg:
+        return loggers
+    for _, lg_conf in logger_cfg.items():
+        if isinstance(lg_conf, DictConfig) and "_target_" in lg_conf:
+            loggers.append(instantiate(lg_conf))
+    return loggers
+
+
+def instantiate_callbacks(callbacks_cfg) -> List[L.Callback]:
+    callbacks = []
+    if not callbacks_cfg:
+        return callbacks
+    for _, cb_conf in callbacks_cfg.items():
+        if isinstance(cb_conf, DictConfig) and "_target_" in cb_conf:
+            callbacks.append(instantiate(cb_conf))
+    return callbacks
 
 
 def count_tensor_params(tensor, dims=None):
